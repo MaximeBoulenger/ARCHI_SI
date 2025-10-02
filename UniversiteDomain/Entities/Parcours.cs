55fed8bd0c61a399adb9637977cfe5fcf9807d1f
Maxime Boulenger <maxime.boulenger@etud.u-picardie.fr>
2025-10-02 13:01:02 +0200

    Push 8.1.1 Présentation

---
+++ UniversiteDomain/Entities/Parcours.cs
@@ -0,0 +1,13 @@
+﻿namespace UniversiteDomain.Entities;
+
+public class Parcours
+{
+    public long Id { get; set; }
+    public string NomParcours { get; set; } = String.Empty;
+    public int AnneeFormation { get; set; }
+
+    public override string ToString()
+    {
+        return $"{NomParcours} {AnneeFormation}";
+    }
+}
---
+++ UniversiteDomain/UseCases/ParcoursUseCases/Create/CreateParcoursUseCase.cs
@@ -0,0 +1,8 @@
+﻿using UniversiteDomain.DataAdapters;
+
+namespace UniversiteDomain.UseCases.ParcoursUseCases.Create;
+
+public class CreateParcoursUseCase(IEtudiantRepository etudiantRepository)
+{
+    
+}
